b92e223e171d135031fe36b08f031565eb7803ac
kess <kevin@kess.dev>
2024-03-17 23:26:33 -0500

    feat: Create interface for Typescript

--- webview-dist/index.d.ts
+++ webview-dist/index.d.ts
@@ -1,1 +1,12 @@
-export declare function execute(): Promise<void>
+export default class Rusqlite {
+    name: string;
+    constructor(name: string);
+    static openInMemory(name: string): Promise<Rusqlite>;
+    static openInPath(path: string): Promise<Rusqlite>;
+    migration(migrations: Migration[]): Promise<void>;
+    update(sql: string, parameters: Map<string, any>): Promise<void>;
+}
+export interface Migration {
+    name: string;
+    sql: string;
+}
